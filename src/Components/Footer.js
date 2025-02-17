5977c030d260b8e0768145eb0dbc50bdf5538eee
DNRDev <cartjonnson@gmail.com>
2025-02-17 15:23:39 +0700

    Update Footer Section

---
+++ src/Components/Footer.js
@@ -0,0 +1,48 @@
+import { Fade } from "react-awesome-reveal";
+
+const { Component } = require("react");
+
+class Footer extends Component{
+    render() {
+        if(!this.props.data) return null;
+
+        const networks = this.props.data.social.map(function(networks) {
+            return(
+                <li key={networks.name}>
+                    <a href={networks.url}>
+                        <i className={networks.className}>
+
+                        </i>
+                    </a>
+                </li>
+            )
+        });
+
+        return(
+            <footer>
+                <div className="row">
+                    <Fade bottom>
+                        <div className="twelve columns">
+                            <ul className="social-links">{networks}</ul>
+                            <ul className="copyright">
+                                <li>&copy; Copyright 2025 Dohar Siregar</li>
+                                <li>
+                                    Design by {""}
+                                    <a title="Styleshout" href="http://www.styleshout.com">
+                                    Styleshout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </Fade>
+                    <div id="go-top">
+                        <a className="smoothscroll" title="Back to Top" href="#home">
+                            <i className="icon-up-open"></i>
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
